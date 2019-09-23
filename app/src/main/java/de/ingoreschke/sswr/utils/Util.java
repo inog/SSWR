46afcc5e874cceb4acb35bf9d26feaf810441b09
inog <kontakt@ingo-reschke.de>
2019-09-23 12:31:09 +0200

    rename

--- app/src/main/java/de/ingoreschke/sswr/utils/Util.java
+++ app/src/main/java/de/ingoreschke/sswr/utils/Util.java
@@ -1,11 +1,11 @@
 package de.ingoreschke.sswr.utils;
 
 public class Util {
-	public static int safeLongToInt(long l) {
-	    if (Integer.MIN_VALUE > l || l > Integer.MAX_VALUE ) {
+	public static int safeLongToInt(long number) {
+	    if (Integer.MIN_VALUE > number || number > Integer.MAX_VALUE ) {
 	        throw new IllegalArgumentException
-	            (l + " cannot be cast to int without changing its value.");
+	            (number + " cannot be cast to int without changing its value.");
 	    }
-	    return (int) l;
+	    return (int) number ;
 	}
 }
