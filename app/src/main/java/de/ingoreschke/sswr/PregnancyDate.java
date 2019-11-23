17e243f214f1c51207458ec45600c8e3993feb3a
inog <kontakt@ingo-reschke.de>
2019-11-23 21:43:14 +0100

    refactor, using Period to get simpler the different in days. unfortunatly not correct jet. Writing a failing test for days over a month
    Signed-off-by: inog <kontakt@ingo-reschke.de>

--- app/src/main/java/de/ingoreschke/sswr/PregnancyDate.java
+++ app/src/main/java/de/ingoreschke/sswr/PregnancyDate.java
@@ -1,5 +1,8 @@
 package de.ingoreschke.sswr;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -22,16 +25,22 @@ public class PregnancyDate {
 
 
 	public PregnancyDate(Date date1, Date date2){
-        Date d1 = eraseTime(date1);
-        Date d2 = eraseTime(date2);
-		daysToBirth = dateDiffInDays(d1, d2);
-		daysUntilNow = GESTATION_IN_DAY - daysToBirth;
+
+    //    Date d2 = eraseTime(date2);
+	//	Date d1 = eraseTime(date1);
+	//	daysToBirth = dateDiffInDays(d1, d2);
+
+		LocalDate ld1 = LocalDate.from(date1.toInstant().atZone(ZoneId.systemDefault()));
+		LocalDate ld2 = LocalDate.from(date2.toInstant().atZone(ZoneId.systemDefault()));
+		daysToBirth = dateDiffInDays(ld1, ld2);
+
+		daysUntilNow = GESTATION_IN_DAY - this.daysToBirth;
 		weeksUntilNow = calcWeeks(daysUntilNow);
 		restOfWeekUntilNow = calcRestOfWeek(daysUntilNow);
 		xteWeek = weeksUntilNow + 1;
 		xteMonth = calcMonth(weeksUntilNow) +1;
 	}
-	
+
 	private Date eraseTime(Date date){
 		Calendar c = GregorianCalendar.getInstance();
 		c.setTime(date);
@@ -39,6 +48,16 @@ public class PregnancyDate {
 		c.set(Calendar.MINUTE, 0);
 		c.set(Calendar.SECOND, 0);
 		return c.getTime();
+	}
+	private long dateDiffInDays(LocalDate date1, LocalDate date2){
+		Period intervalPeriod = Period.between(date1, date2);
+		int days = intervalPeriod.getDays();
+		if (days > GESTATION_IN_DAY + 22L){
+			throw new IllegalArgumentException(DATE2_TOO_BIG);
+		}else if(days < -21L){
+			throw new IllegalArgumentException(DATE1_TOO_SMALL);
+		}
+		return days;
 	}
 	
 	private long dateDiffInDays(Date date1, Date date2){
@@ -51,7 +70,8 @@ public class PregnancyDate {
 		long milliseconds = md2 - md1;
 		long days = Math.round(milliseconds / MILLISECONDS_IN_DAY);
 		Log.d(TAG, "dateDiffInDays : " + date1.toString() + "\t" + date2.toString() + "\t" + days) ;
-		
+
+
 		if (days > GESTATION_IN_DAY + 22L){
 			throw new IllegalArgumentException(DATE2_TOO_BIG);
 		}else if(days < -21L){
--- app/src/test/java/de/ingoreschke/sswr/PregnancyDateTest.java
+++ app/src/test/java/de/ingoreschke/sswr/PregnancyDateTest.java
@@ -28,4 +28,44 @@ public class PregnancyDateTest {
         assertEquals(39L, cut.getXteWeek());
         assertEquals(10L, cut.getXteMonth());
     }
+
+
+    @Test
+    public void testPregnancyDate_differentTime() {
+        long expectedDays2Birth = 10L;
+        long expectedDaysUntilNow = PregnancyDate.GESTATION_IN_DAY - expectedDays2Birth;
+        LocalDate now = LocalDate.now();
+        LocalDate birthDate = now.plusDays(expectedDays2Birth);
+
+        Date startDate = new Date();
+        Date dayOfBirth = Date.from(birthDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        PregnancyDate cut = new PregnancyDate(startDate, dayOfBirth);
+
+        assertEquals(expectedDays2Birth, cut.getDaysToBirth());
+        assertEquals(expectedDaysUntilNow, cut.getDaysUntilNow());
+        assertEquals(4L, cut.getRestOfWeekUntilNow());
+        assertEquals(38L, cut.getWeeksUntilNow());
+        assertEquals(39L, cut.getXteWeek());
+        assertEquals(10L, cut.getXteMonth());
+    }
+    @Test
+    public void testPregnancyDate_later() {
+        long expectedDays2Birth = 100L;
+        long expectedDaysUntilNow = PregnancyDate.GESTATION_IN_DAY - expectedDays2Birth;
+        LocalDate now = LocalDate.now();
+        LocalDate birthDate = now.plusDays(expectedDays2Birth);
+
+        Date startDate = new Date();
+        Date dayOfBirth = Date.from(birthDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        PregnancyDate cut = new PregnancyDate(startDate, dayOfBirth);
+
+        assertEquals(expectedDays2Birth, cut.getDaysToBirth());
+        assertEquals(expectedDaysUntilNow, cut.getDaysUntilNow());
+        assertEquals(4L, cut.getRestOfWeekUntilNow());
+        assertEquals(38L, cut.getWeeksUntilNow());
+        assertEquals(39L, cut.getXteWeek());
+        assertEquals(10L, cut.getXteMonth());
+    }
+
+
 }
