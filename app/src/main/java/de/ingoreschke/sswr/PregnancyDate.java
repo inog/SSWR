4f9e72ab1889533e66aea27938e1074e7fdba2ab
inog <kontakt@ingo-reschke.de>
2020-11-26 22:24:21 +0100

    remove obsolete tests

--- app/src/main/java/de/ingoreschke/sswr/PregnancyDate.java
+++ app/src/main/java/de/ingoreschke/sswr/PregnancyDate.java
@@ -4,17 +4,16 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class PregnancyDate {
-    private static final String TAG = "PregnancyDate";
 
     static final long GESTATION_IN_DAY = 280L;
     public static final String DATE2_TOO_BIG = "Date (d2) is too far in the future.";
     public static final String DATE1_TOO_SMALL = "Date (d1) is too far in the past.";
-    private long daysToBirth;
-    private long daysUntilNow;
-    private long weeksUntilNow;
-    private long restOfWeekUntilNow;
-    private long xteWeek;
-    private long xteMonth;
+    private final long daysToBirth;
+    private final long daysUntilNow;
+    private final long weeksUntilNow;
+    private final long restOfWeekUntilNow;
+    private final long xteWeek;
+    private final long xteMonth;
 
     public PregnancyDate(LocalDate date1, LocalDate date2) {
         daysToBirth = dateDiffInDays(date1,date2);
--- app/src/test/java/de/ingoreschke/sswr/PregnancyDateTest.java
+++ app/src/test/java/de/ingoreschke/sswr/PregnancyDateTest.java
@@ -3,10 +3,6 @@ package de.ingoreschke.sswr;
 import org.junit.Test;
 
 import java.time.LocalDate;
-import java.time.Month;
-import java.time.ZoneId;
-import java.util.Calendar;
-import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 
@@ -29,26 +25,8 @@ public class PregnancyDateTest {
         assertEquals(10L, cut.getXteMonth());
     }
 
-
     @Test
-    public void testPregnancyDate_differentTime() {
-        long expectedDays2Birth = 10L;
-        long expectedDaysUntilNow = PregnancyDate.GESTATION_IN_DAY - expectedDays2Birth;
-        LocalDate now = LocalDate.now();
-        LocalDate birthDate = now.plusDays(expectedDays2Birth);
-
-        PregnancyDate cut = new PregnancyDate(now, birthDate);
-
-        assertEquals(expectedDays2Birth, cut.getDaysToBirth());
-        assertEquals(expectedDaysUntilNow, cut.getDaysUntilNow());
-        assertEquals(4L, cut.getRestOfWeekUntilNow());
-        assertEquals(38L, cut.getWeeksUntilNow());
-        assertEquals(39L, cut.getXteWeek());
-        assertEquals(10L, cut.getXteMonth());
-    }
-
-    @Test
-    public void testPregnancyDate_later() {
+    public void testPregnancyDate_100DaysToWait() {
         long expectedDays2Birth = 100L;
         long expectedDaysUntilNow = PregnancyDate.GESTATION_IN_DAY - expectedDays2Birth;
         LocalDate now = LocalDate.now();
@@ -64,26 +42,19 @@ public class PregnancyDateTest {
         assertEquals(7, cut.getXteMonth());
     }
 
-    @Test (expected = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testPregnancyDate_Date2ToBig() {
         LocalDate now = LocalDate.now();
         LocalDate birthDate = now.plusDays(350L);
         new PregnancyDate(now, birthDate);
     }
 
-    @Test (expected = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testPregnancyDate_Date1ToSmall() {
         LocalDate now = LocalDate.now();
         LocalDate birthDate = now.minusDays(22L);
-        new PregnancyDate(now,birthDate);
+        new PregnancyDate(now, birthDate);
     }
 
-    @Test
-    public void testCalenderDate(){
-        Calendar c = Calendar.getInstance();
-        System.out.println(c.get(Calendar.YEAR) + " - " + c.get(Calendar.MONTH) + " - " + c.get(Calendar.DAY_OF_MONTH));
-        System.out.println(c.getTime());
-        System.out.println(LocalDate.of(c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH)));
-        System.out.println(LocalDate.of(c.get(Calendar.YEAR), c.get(Calendar.MONTH)+1, c.get(Calendar.DAY_OF_MONTH))); //need +1 for no Zero Month
-    }
+
 }
