07716b3a3a0e699bbd04cacecaab8d1cf0351e36
inog <kontakt@ingo-reschke.de>
2020-11-28 16:48:26 +0100

    cleanup

--- app/src/androidTest/java/de/ingoreschke/sswr/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/de/ingoreschke/sswr/ExampleInstrumentedTest.java
@@ -1,26 +1,24 @@
 package de.ingoreschke.sswr;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("de.ingoreschke.sswr", appContext.getPackageName());
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("de.ingoreschke.sswr.lite", appContext.getPackageName());
     }
 }
