3a4ff10d5e63d05b4348af96422c938a7d19237d
Ingo Reschke <kontakt@ingo-reschke.de>
2025-12-30 00:36:52 +0100

    remove the flavor way of seperate build.s
    Signed-off-by: Ingo Reschke <kontakt@ingo-reschke.de>

--- app/src/main/java/de/ingoreschke/sswr/ActivityIr.java
+++ app/src/main/java/de/ingoreschke/sswr/ActivityIr.java
@@ -1,18 +1,13 @@
 package de.ingoreschke.sswr;
 
-
-import android.util.Log;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class ActivityIr extends AppCompatActivity {
-    protected static final String AD_UNIT_ID_MAIN = "ca-app-pub-1283865206002218/4256611081";//"a14e80f4900e534";
+    protected static final String AD_UNIT_ID_MAIN = "ca-app-pub-1283865206002218/4256611081";
     protected static final String AD_UNIT_ID_WEEKINFO = "ca-app-pub-1283865206002218/8768072283";
     protected static final int FULL_VERSION_REQUIRED = -1;
 
-	public boolean isLiteVersion() {
-		String TAG = "ActivityIr";
-		Log.d(TAG, getPackageName());
-        return getPackageName().toLowerCase().contains("lite");
+    public boolean isLiteVersion() {
+        return BuildConfig.SHOW_ADS;
     }
 }
