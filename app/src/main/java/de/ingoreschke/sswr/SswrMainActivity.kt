c9da7c3d0e3a8c6d8db0d0b2bd6ac01dcfd0a18e
Ingo Reschke <kontakt@ingo-reschke.de>
2026-08-14 00:21:36 +0200

    fix(widget): register AppWidget receiver and provider metadata
    - Register Widget receiver in AndroidManifest.xml with APPWIDGET_UPDATE action
    - Add res/xml/widget_info.xml for AppWidgetProviderInfo configuration
    - Modernize widget_layout.xml and widget_shape.xml styling with rounded corners
    - Ensure click PendingIntent is set when updating widget from SswrMainActivity

--- app/src/main/java/de/ingoreschke/sswr/SswrMainActivity.kt
+++ app/src/main/java/de/ingoreschke/sswr/SswrMainActivity.kt
@@ -2,6 +2,7 @@ package de.ingoreschke.sswr
 
 import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.ComponentName
 import android.content.Intent
@@ -416,13 +417,18 @@ class SswrMainActivity : ActivityIr() {
 
     private fun updateWidget() {
         Log.d(TAG, "updateWidget")
-        val week = pregnancyDate!!.weeksUntilNow.toString() + " + " + pregnancyDate!!.restOfWeekUntilNow.toString()
-        val xteWeek = pregnancyDate!!.xteWeek.toString() + getString(R.string.str_xteWeek_suffix)
-        val appWidgetManager = AppWidgetManager.getInstance(this) //this is context
+        val date = pregnancyDate ?: return
+        val week = "${date.weeksUntilNow} + ${date.restOfWeekUntilNow}"
+        val xteWeek = "${date.xteWeek}${getString(R.string.str_xteWeek_suffix)}"
+        val appWidgetManager = AppWidgetManager.getInstance(this)
         val views = RemoteViews(this.packageName, R.layout.widget_layout)
-        //Perform update on the view
         views.setTextViewText(R.id.widgetTV02, week)
         views.setTextViewText(R.id.widgetTV03, xteWeek)
+
+        val intent = Intent(this, SswrMainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        views.setOnClickPendingIntent(R.id.widgetCanvas, pendingIntent)
+
         appWidgetManager.updateAppWidget(
                 ComponentName(this, Widget::class.java),
                 views
